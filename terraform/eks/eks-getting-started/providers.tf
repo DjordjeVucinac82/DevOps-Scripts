582d297093cb243b7f82ed3743c47f12eaf6d392
DjordjeVucinac82 <djordje.vucinac@gmail.com>
2022-08-15 09:51:28 +0200

    terraform eks

---
+++ terraform/eks/eks-getting-started/providers.tf
@@ -0,0 +1,25 @@
+# terraform {
+#   required_version = ">= 0.12"
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "awsdjole"
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
---
+++ terraform/eks/eks-getting-started/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
